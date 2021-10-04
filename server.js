867fbd949c71f5f4041674520e13c6018216904d
Aldo Solano <aldoglez34@gmail.com>
2021-10-04 10:02:09 -0500

    first commit

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const _ = require("lodash");
 
-if (process.env.NODE_ENV === "production") {
+if (_.isEqual(process.env.NODE_ENV, "production"))
   app.use(express.static("client/build"));
-}
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
+);
 
-app.listen(PORT, function () {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`🌎 ==> API server now on port ${PORT}!`));
